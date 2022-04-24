a670a3ec9aeb618e3a9dac700b185b50d1d9dc95
world-one <worldone85@gmail.com>
2022-04-24 15:57:54 +0900

    feat: add isEmpty

---
+++ utils/isEmpty.js
@@ -0,0 +1,23 @@
+function isEmpty(target) {
+  if (target === undefined || target === null) return target;
+  switch (target.constructor) {
+    case Object:
+      return isEmptyObject(target);
+    case Array:
+      return isEmptyArray(target);
+    case String:
+      return target.trim().length === 0;
+    case Number: return false;
+    default: return target
+  }
+}
+
+export default isEmpty;
+
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+function isEmptyArray(arr) {
+  return arr.length === 0;
+}
